484fbf17e268c9d7b53b8d4340de4a3bccaf488e
carlossantos01 <carlossantosgithub@gmail.com>
2026-03-26 21:43:46 -0300

    Add files via upload

---
+++ primeiro-projeto/src/components/Mensagem/index.jsx
@@ -0,0 +1,12 @@
+import './style.css'
+
+function Mensagem() {
+    return (
+        <div className='mensagem'>
+        <h1>Componente</h1>
+        <p>Este é um componente de simples.</p>
+    </div>
+)
+}
+
+export default Mensagem
